621d4ada348d2dd7896740c7f4efe0849c78cbfb
Georgina Lin <xialin1@g.ucla.edu>
2022-11-21 14:13:08 -0600

    group features framework

---
+++ client/src/components/GroupComponents/ConcertInfo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Attendees from './Attendees.jsx';
+import ConcertDetails from './ConcertDetails.jsx';
+
+const ConcertInfo = () => {
+
+  return (
+
+  <div>
+    <ConcertDetails/>
+    <Attendees/>
+  </div>
+
+  )
+}
+
+export default ConcertInfo;
---
+++ client/src/components/GroupComponents/index.js
@@ -0,0 +1,10 @@
+import ConcertInfo from './ConcertInfo.jsx';
+import Attendees from './Attendees.jsx';
+import Chat from './Chat.jsx';
+
+
+export {
+  ConcertInfo,
+  Attendees,
+  Chat
+}
