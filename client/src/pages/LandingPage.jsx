f2113b8e3eea50be7ce1ede50721683f11809764
gthellter <drewsworld16@gmail.com>
2022-11-21 09:51:50 -0600

    Finished Login Buttons

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,14 +1,43 @@
 import axios from 'axios';
 import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+height: 100vh;
+display: flex;
+justify-content: center;
+align-items: center;
+`;
+
+
+const Login = styled.button`
+  display: flex;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.5);
+  color: #fff;
+  line-height: 42px;
+  padding: 0;
+  border: none;
+  padding: 10px;
+  margin: 5px;
+  border-radius: 10px;
+
+  &:hover {
+    background: rgba(0, 0, 0, 1);
+  }
+`;
 
 const LandingPage = () => {
 
   return (
-    <div>
-      <a href='http://localhost:8080/spotify/auth/login'>
+    <Div>
+      <Login onClick={() => {window.location.href='http://localhost:8080/spotify/auth/login'}}>
         Log In With Spotify
-      </a>
-    </div>
+      </Login>
+      <Login onClick={() => {window.location.href='https://www.spotify.com/us/signup'}}>
+        Sign Up With Spotify
+      </Login>
+    </Div>
   )
 }
 
