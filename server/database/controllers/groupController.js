0aa30349251fbe400b407b29fb2d800529b259f8
Gabriela Olarte <gmolarte@gmail.com>
2022-11-22 17:39:58 -0600

    homefeed: server routes

--- server/database/controllers/groupController.js
+++ server/database/controllers/groupController.js
@@ -1,9 +1,23 @@
-const { pool } = require('../postgresDB.js');
+const pool = require('../postgresDB.js');
 
 const getGroups = (req, res) => {
   console.log('getting groups', req.query);
-  res.status(200);
-  res.send('Got the groups');
+  const groupsQuery = `SELECT *
+  FROM groups g
+  LEFT JOIN members m
+  ON m.group_id=g.group_id
+  WHERE m.individual_id=12345
+  ORDER BY datetime_local ASC`;
+  pool.query(groupsQuery)
+  .then((result) => {
+    res.status(200);
+    res.send(result.rows);
+  })
+  .catch(e => {
+    console.error(e.stack);
+    res.status(404);
+    res.send(e.stack);
+  });
 }
 
 module.exports = {
