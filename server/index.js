fc3cb896d4b1fcab76abebe2f42f072cfd9302ef
Gabriela Olarte <gmolarte@gmail.com>
2022-11-22 11:42:33 -0600

    implement route to get users groups

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const express = require('express');
 
 const spotifyAuth = require("./routes/spotifyAuth.js");
 const sgAuth = require("./routes/sg.js");
+const { getGroups } = require("./database/controllers/groupController");
 
 const cookieParser = require('./middleware/cookieParser.js');
 const app = express();
@@ -14,7 +15,7 @@ const PORT = process.env.PORT;
 const SERVER_ADDR = process.env.SERVER_ADDR;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true} ));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser);
 app.use('/spotify/auth', spotifyAuth);
@@ -23,9 +24,10 @@ app.use('/sg', sgAuth);
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+app.get('/db/groups', getGroups);
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, "../client/dist", 'index.html'));
-  });
+});
 
 app.listen(PORT, () => {
   console.log(`listening at ${SERVER_ADDR}:${PORT}`);
