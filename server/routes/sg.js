bb0cf2a82d46294c5cf93d2956cab2615fea1118
Ben Tanaka <BenT07891@gmail.com>
2022-11-23 12:25:31 -0600

    search should be functional as well as db group creation

--- server/routes/sg.js
+++ server/routes/sg.js
@@ -7,28 +7,24 @@ const getGroupOrCreateNewFunc = require('../database/controllers/getGroupOrCreat
 let eventTypes = 'taxonomies.name=club_passes&taxonomies.name=music_festival&taxonomies.name=Concert&taxonomies.name=Concerts';
 
 router.get('/events', (req, res) => {
-  //{state, city, eventArtistSearchTerm, minPrice, maxPrice, IP_address}
-  // console.log(req.query)
   let argsConversion = {per_page: 50};
-  if (req.query.IP_address != undefined) {
-      argsConversion['geoip'] = req.query.IP_address
-  } else {
-    if (req.query.state != undefined) {
-      argsConversion['venue.state'] = req.query.state
-    }
-    if (req.query.city != undefined) {
-      argsConversion['venue.city'] = req.query.city
-    }
+
+  if (req.query.state != '') {
+    argsConversion['venue.state'] = req.query.state
   }
-  if (req.query.eventArtistSearchTerm != undefined) {
+  if (req.query.city != '') {
+    argsConversion['venue.city'] = req.query.city
+  }
+  if (req.query.eventArtistSearchTerm != '') {
     argsConversion['q'] = req.query.eventArtistSearchTerm
   }
-  if (req.query.maxPrice != undefined) {
+  if (req.query.maxPrice != '') {
     argsConversion['highest_price.lte'] = req.query.maxPrice
   }
-  if (req.query.minPrice != undefined) {
+  if (req.query.minPrice != '') {
     argsConversion['lowest_price.gt'] = req.query.minPrice
   }
+
   return axios({
     method:'get',
     url: `https://api.seatgeek.com/2/events?client_id=${process.env.SG_CLIENT_ID}&client_secret=${process.env.SG_CLIENT_SECRET}&${eventTypes}`,
@@ -48,15 +44,39 @@ router.get('/events', (req, res) => {
       country: item.venue.country,
       state: item.venue.state,
       postal_code: item.venue.postal_code,
+      address: item.venue.address,
+      extended_address: item.venue.extended_address,
       location_latitude: item.venue.location.lat,
       location_longitude: item.venue.location.lon,
-      average_price: item.stats.average_price
+      average_price: item.stats.average_price,
+
     }})
-    if (req.query.eventArtistSearchTerm != undefined) {
-      slimmedResults = slimmedResults.filter((item)=> {
+    // if (req.query.eventArtistSearchTerm != '') {
+    //   slimmedResults = slimmedResults.filter((item)=> {
+    //     return item.event_title.toUpperCase().includes(req.query.eventArtistSearchTerm.toUpperCase()) || item.performer.some((item)=>{return item.name.toUpperCase().includes(req.query.eventArtistSearchTerm.toUpperCase())})
+    //   })
+    // }
+
+    slimmedResults = slimmedResults.filter((item)=> {
+      if (item.average_price === null) {
+        return false;
+      }
+      if (req.query.eventArtistSearchTerm != '') {
         return item.event_title.toUpperCase().includes(req.query.eventArtistSearchTerm.toUpperCase()) || item.performer.some((item)=>{return item.name.toUpperCase().includes(req.query.eventArtistSearchTerm.toUpperCase())})
-      })
-    }
+      } else {
+        return true;
+      }
+    })
+
+
+    // slimmedResults = slimmedResults.filter((item)=> {
+    //   if (req.query.eventArtistSearchTerm === '') {
+    //     return item.average_price != null
+    //   } else {
+    //     return (item.event_title.toUpperCase().includes(req.query.eventArtistSearchTerm.toUpperCase()) || item.performer.some((item)=>{return item.name.toUpperCase().includes(req.query.eventArtistSearchTerm.toUpperCase())})) && item.average_price != null
+    //   }
+    // })
+
     res.send(slimmedResults)
   })
   .catch((err)=>{res.status(500).send(err)})
@@ -64,7 +84,7 @@ router.get('/events', (req, res) => {
 
 router.post('/events', (req, res) => {
 
-  getGroupOrCreateNewFunc(req.body.individual_id, req.body.objEventData)
+  getGroupOrCreateNewFunc(req.body.invididual_id, req.body.objEventData)
   .then((val)=> {res.send(val)})
   .catch((err)=>{res.status(500).send()});
 })
