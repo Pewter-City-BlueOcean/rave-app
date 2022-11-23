ced50e6cbe10d3cfd90c278f235c5dc232871fff
gthellter <drewsworld16@gmail.com>
2022-11-23 16:10:24 -0600

    added groups

--- client/src/helpers/raveStore.js
+++ client/src/helpers/raveStore.js
@@ -2,6 +2,7 @@ import create from 'zustand';
 
 export const useRaveStore = create(set => ({
   currentGroup: {},
+  groups: [],
   userId: '',
   profileViewing: {},
   setCurrentGroup: (group) => {
