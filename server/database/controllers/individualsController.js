9c65d7c9484eebc479e754a7ec3bf81a99bbb6ae
gthellter <drewsworld16@gmail.com>
2022-11-23 09:22:16 -0600

    handled overwritting profile Images

--- server/database/controllers/individualsController.js
+++ server/database/controllers/individualsController.js
@@ -3,21 +3,25 @@ var fs = require('fs/promises');
 const path = require("path");
 
 const updateIndividual = (req, res) => {
+
   const query = `
 
   `;
   //Write image to fileSystem
   const file = req.files.photo;
   // let fileExtension = req.files.photo.name.split('.')[1];
-  file.name = req.body.username + path.extname(req.files.photo.name);
-  fs.truncate(path.join(__dirname, '../..', '/uploads/images/', file.name), 0).then (() => {
-    return fs.writeFile(path.join(__dirname, '../..', '/uploads/images/', file.name), file.data).catch(err => {
-      console.log(err);
+  const filename = req.body.username + path.extname(req.files.photo.name);
+  const directory = path.join(__dirname, '../..', '/uploads/images/', filename);
+    fs.open(directory, 'w+').then(() => {
+      return fs.truncate(directory, 0).then(() => {
+        return fs.writeFile(directory, file.data).catch(err => {
+          console.log(err);
+        });
+      }).catch(err => {
+        console.log(err);
+      })
     })
-  }).catch(err => {
-    console.log(err);
-  })
-}
+  }
 
 module.exports = {
   updateIndividual
